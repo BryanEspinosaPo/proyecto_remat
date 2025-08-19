aa5b1b8a07412ddd9e4e1421192b6d2e094ff547
Bryan Espinosa <byaespinosag@poligran.edu.co>
2025-08-18 20:04:20 -0500

    correcccion de ruta

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReporteCliController;
 
-Route::get('/', function () {
-    return view('landing');
-});
 Route::get('/agendamiento', function () {
     return view('agendamiento');
-
-    Route::get('/reporte/{id}', [ReporteCliController::class, 'show'])
-        ->whereNumber('id')
-        ->name('reporte.usuario');
-
-    Route::get('/logout', fn() => redirect('/'))->name('logout');
-
-
-    Route::get('/reporte/{id}/pdf', [ReporteCliController::class, 'descargarPDF'])
-        ->whereNumber('id')
-        ->name('reporte.pdf');
 });
-Route::get('/puntos', function () {
-    return view('puntos');
-});
+
+Route::get('/reporte/{id}', [ReporteCliController::class, 'show'])
+    ->whereNumber('id')
+    ->name('reporte.usuario');
+
+Route::get('/logout', fn() => redirect('/'))->name('logout');
+
+
+Route::get('/reporte/{id}/pdf', [ReporteCliController::class, 'descargarPDF'])
+    ->whereNumber('id')
+    ->name('reporte.pdf');
